072fdf7a272372b4baae19bea943132805d4965c
YessLarco <yesselaran@gmail.com>
2016-11-30 14:56:46 -0500

    usuarios
    usuarios enviados con parámetros

--- myapp/app.js
+++ myapp/app.js
@@ -1,50 +1,100 @@
 var express = require('express')
 var app = express()
+var puerto = 5050
+
+var usuarios = [
+    {
+        id: 1,
+        nombre: 'Yesse',
+        cedula: '1722741061'
+    },
+    
+    {
+        id: 2,
+        nombre: 'Carlos',
+        cedula: '112548569'
+    },
+    
+    {
+        id: 3,
+        nombre: 'Juan',
+        cedula: '125485691'
+    }
+]
+
+var contador=3;
 
 app.get('/TecnologiasWeb', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.post('/TecnologiasWeb', function (req, res) {
-  //res.send('con post!!!!')
-  
-  var usuario={
-      nombre:'Yessenia',
-      cedula:'1722741061'
-  }
-//  
-//  usuario = {
-//      nombre:usuario.nombre,
-//      cedula:usuario.cedula,
-//      apellido: 'Larco'
-//  }
-  
-  usuario.apellido = 'Larco';
-    usuario.mascotas = [];
-    usuario.casado = false;
-  
-  console.log('Lo que tengo en el REQUEST es: ')
-  console.log(req)
-  console.log('Lo que tengo en el RESPONSE es: ')
-  console.log(res)
-  
-  console.log('Lo que tengo en el CABECERAS DE REQUEST es: ')
-  console.log(req.headers);
-  console.log('Lo que tengo en el CABECERAS DE RESPONSE es: ')
-  console.log(res.headers);
-  
-  //res.send('nombre: Yessenia, cedula:1722741061');
-  
-    res.headers ={
-        token:1234
+app.get('/Usuario/:id',function(req, res){
+        
+    var idActual = req.params.id;
+    
+    //se busca en todo el arreglo de usuarios el id que se da como parámetro
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
     }
     
-    res.append('token','1234');
-  res.json(usuario);
-  
+    //responde que no existe el usuario luego de busar en el arreglo
+    res.send('No existe el usuario!!!');
+        })
+
+app.get('/Usuario',function(req, res){
+     
+    console.log(req.query.nombre);
+    
+    console.log(req.query.cedula);
+    
+    if(!req.query.nombre){
+        res.send('No envio el nombre')
+        
+    }
+    
+    if(!req.query.cedula){
+        res.send('No envio la cedula')
+    }
+    
+    var nuevoUsuario = {
+        id:contador+1,
+        nombre:req.query.nombre,
+        cedula:req.query.cedula
+    }
+    
+    usuarios.push(nuevoUsuario);
+    
+    contador = contador++;
+    res.json(nuevoUsuario)
+//    
+//    console.log(req.param('nombre'));
+//    
+//    //Buscamos el parametro nombre
+//    console.log(req.query.nombre);
+//    
+//    res.json(usuarios);
+        })
+
+app.post('/TecnologiasWeb', function (req, res) {
+    //res.send('con post!!!!')
+
+    var parametros = req.params;
+
+    console.log(parametros);
+
+    var usuario = {
+        nombre: 'Yessenia',
+        cedula: '1722741061'
+    }
+
+
+
+    res.json(usuario);
+
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(puerto, function () {
+    console.log('Example app listening on port '+puerto+'!')
 })
-
