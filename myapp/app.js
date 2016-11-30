b1956a9076373def6132aa6ad251c0b0c9233b6c
YessLarco <yesselaran@gmail.com>
2016-11-30 14:01:17 -0500

    post
    ejemplo con post!!!

--- myapp/app.js
+++ myapp/app.js
@@ -5,6 +5,24 @@ app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(5050, function () {
-  console.log('Example app listening on port 5050!')
+app.post('/TecnologiasWeb', function (req, res) {
+  res.send('con post!!!!')
+  
+  console.log('Lo que tengo en el REQUEST es: ')
+  console.log(req)
+  console.log('Lo que tengo en el RESPONSE es: ')
+  console.log(res)
+  
+  console.log('Lo que tengo en el CABECERAS DE REQUEST es: ')
+  console.log(req.headers)
+  console.log('Lo que tengo en el CABECERAS DE RESPONSE es: ')
+  console.log(res.headers)
+  
+  res.send('nombre: Yessenia, cedula:1722741061')
+  
 })
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
