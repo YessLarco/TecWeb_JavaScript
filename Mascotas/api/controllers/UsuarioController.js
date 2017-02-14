b2b3412ed2888d8e08df7b855c79fb65ecf70c93
YessLarco <yesselaran@gmail.com>
2017-02-14 14:51:28 -0500

    Controlador
    cambiar el controlador

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -6,48 +6,21 @@
  */
 
 module.exports = {
-    
-    
-          if(req.method=="POST"){
-            
-        }else{
-            
-        }
 
-         
-     },
-    
-//    CrearUsuarioForm: function(req, res){
-//        
-//        var parametros = req.allParams();
-//         
-//         console.log(parametros);
-//         
-//
-//         if(parametros.nombres&&parametros.apellidos){
-//             //crear usuario
-//             
-//              Usuario.create({
-//             nombres:parametros.nombres,
-//             apellidos: parametros.apellidos,
-//             correo: parametros.correo
-//         }).exec(function (error, usuarioCreado){
-//             
-//             
-//               if (error) { return res.serverError(); }
-//             
-//             sails.log.info(usuarioCreado);
-//               
-//             return res.view('vistas/home');
-//         });
-//             
-//         } else {
-//             
-//             //bad request
-//             return res.badRequest('No envia todos los parametros');
-//         }
-//        
-//    }
-	
+    crearUsuario: function (req, res) {
+
+        if(req.method=="POST"){
+
+            return res.view('vistas/Error',{
+                error:{
+                    desripcion:"Error en el uso del Metodo HTTP",
+                    rawError:"HTTP Invalido",
+                    url:"/CrearUsuario"
+                }
+            });
+        }
+    }
+
+
 };
 
