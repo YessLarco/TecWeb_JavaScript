6325b79f5ada0e473011334dd5c38848b5ccefe9
YessLarco <yesselaran@gmail.com>
2017-02-15 13:52:31 -0500

    Listar Usuarios
    Listar los usurios, cambios en el controlador y la vista

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -38,10 +38,24 @@ module.exports = {
                         });
                     }
 
-                    return res.view('vistas/Usuario/crearUsuario');
+                     Usuario.find()
+                        .exec(function (errorIndefinido, usuariosEncontrados) {
 
+                            if (errorIndefinido) {
+                                res.view('vistas/Error', {
+                                    error: {
+                                        desripcion: "Hubo un problema cargando los Usuarios",
+                                        rawError: errorIndefinido,
+                                        url: "/ListarUsuarios"
+                                    }
+                                });
+                            }
 
-                })
+                            res.view('vistas/Usuario/ListarUsuarios', {
+                                usuarios: usuariosEncontrados
+                            });
+                        })
+
 
 
             } else {
